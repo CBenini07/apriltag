839f9185332a4a4803e7418a9725616be9970c50
CBenini07 <cauabenini07@hotmail.com>
2024-07-31 18:31:05 -0300

    modificação para desenhar o chessboard no momento da coleta de imagens (facilita a visualização)

--- camera_calibration/image_collect.py
+++ camera_calibration/image_collect.py
@@ -2,6 +2,10 @@ import cv2
 import os
 import time
 
+# Defina o tamanho do tabuleiro de xadrez
+chessboard_size = (8, 6)  # número de quadrados internos (cantos)
+square_size = 0.023  # tamanho de cada quadrado do tabuleiro de xadrez em metros
+
 # Nome da pasta onde as imagens serão salvas
 output_folder = './calibration_images'
 
@@ -27,14 +31,16 @@ while True:
         print("Erro: Nao foi possivel capturar o quadro.")
         break
 
-    cv2.imshow('frame', frame)
+    ret, corners = cv2.findChessboardCorners(frame, chessboard_size, None)
+    frame_detected = cv2.drawChessboardCorners(frame, chessboard_size, corners, ret)
+    cv2.imshow('frame', frame_detected)
     # Salva a imagem na pasta especificada
     cv2.imwrite(os.path.join(output_folder, f'frame_{frame_count}.png'), frame)
     frame_count += 1
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    time.sleep(0.3) # def o framerate 
+    time.sleep(0.2) # def o framerate 
     
 cap.release()
 cv2.destroyAllWindows()
